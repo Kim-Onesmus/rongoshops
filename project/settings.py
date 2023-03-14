081e6bdc8436ec797ec92db7edef997de544e86b
kimones1122@gmail.com <kimanzi@localhost.localdomain>
2023-03-14 19:02:05 +0300

    passwaor reset

--- project/settings.py
+++ project/settings.py
@@ -137,5 +137,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'kimonesmuske@gmail.com'
-EMAIL_HOST_PASSWORD = 'ettagjmpqsvaoevr'
+EMAIL_HOST_USER = 'onesmuskimanzi254@gmail.com'
+EMAIL_HOST_PASSWORD = 'tiywbfhlnwqifsgp'
