311a80c4973668259851c09dcb1dfe99f3e3ee6f
Kim-Onesmus <kimones1122@gmail.com>
2023-06-30 15:20:43 +0300

    updates

--- project/settings.py
+++ project/settings.py
@@ -143,3 +143,15 @@ EMAIL_HOST_USER = 'onesmuskimanzi254@gmail.com'
 EMAIL_HOST_PASSWORD = 'tiywbfhlnwqifsgp'
 EMAIL_USE_SSL = False
 
+
+try:
+    from django.contrib.messages import constants as messages
+    MESSAGE_TAGS ={
+        messages.DEBUG: 'alert-info',
+        messages.INFO: 'alert-info',
+        messages.SUCCESS: 'alert-success',
+        messages.WARNING: 'alert-warning',
+        messages.ERROR: 'alert-danger',
+    }
+except Exception as e:
+    pass
